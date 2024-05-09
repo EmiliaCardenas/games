71a8a38c27252ffeede63a163569d7ccb166f54d
Emilia Cardenas <a01706807@tec.mx>
2024-05-09 17:01:00 -0600

    Modificacion connecta

--- connecta.py
+++ connecta.py
@@ -1,7 +1,8 @@
+import turtle
+from freegames import line
+
 """Connect Four
-
-Exercises
-
+   Exercises
 1. Change the colors.
 2. Draw squares instead of circles for open spaces.
 3. Add logic to detect a full row.
@@ -9,9 +10,7 @@ Exercises
 5. How would you detect a winner?
 """
 
-from turtle import *
-
-from freegames import line
+t = turtle.Turtle()
 
 turns = {'red': 'yellow', 'yellow': 'red'}
 state = {'player': 'yellow', 'rows': [0] * 8}
@@ -19,18 +18,18 @@ state = {'player': 'yellow', 'rows': [0] * 8}
 
 def grid():
     """Draw Connect Four grid."""
-    bgcolor('light blue')
+    t.bgcolor('light blue')
 
     for x in range(-150, 200, 50):
         line(x, -200, x, 200)
 
     for x in range(-175, 200, 50):
         for y in range(-175, 200, 50):
-            up()
-            goto(x, y)
-            dot(40, 'white')
+            t.up()
+            t.goto(x, y)
+            t.dot(40, 'white')
 
-    update()
+    t.update()
 
 
 def tap(x, y):
@@ -44,18 +43,18 @@ def tap(x, y):
     x = ((x + 200) // 50) * 50 - 200 + 25
     y = count * 50 - 200 + 25
 
-    up()
-    goto(x, y)
-    dot(40, player)
-    update()
+    t.up()
+    t.goto(x, y)
+    t.dot(40, player)
+    t.update()
 
     rows[row] = count + 1
     state['player'] = turns[player]
 
 
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
-grid()
-onscreenclick(tap)
-done()
+t.setup(420, 420, 370, 0)
+t.hideturtle()
+t.tracer(False)
+t.grid()
+t.onscreenclick(tap)
+t.done()
